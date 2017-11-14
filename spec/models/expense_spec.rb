2831197e6452a49dd1842c22920887ca8ae04daf
Matthew Bussa <matthewbussa@gmail.com>
2017-11-13 21:23:41 -0500

    validate presence of date, merchant, and total

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:merchant) }
+  it { should validate_presence_of(:total) }
+  
 end
